be43ee846057e0b7c904bc91089206cc4d6018cb
trollmii <trollmii@outlook.de>
2025-04-05 17:53:42 +0200

    performance

--- isobiscuit_engine/_engine.pyx
+++ isobiscuit_engine/_engine.pyx
@@ -22,6 +22,9 @@ cdef int hardware_memory_addresses_len = 9
 
 
 cdef object engine_lock_fs = threading.Lock()
+
+
+
 """Engine class"""
 cdef class Engine:
     cdef int mode
@@ -33,7 +36,7 @@ cdef class Engine:
     cdef int pc
     cdef object zip
     cdef bint debug
-    cdef dict register
+    cdef list register
     cdef dict memory
     cdef dict flags
     cdef object stop_event
@@ -43,18 +46,17 @@ cdef class Engine:
         self.mode = 0x12
         self.zip: io.BytesIO = zip
         self.stack = []
-        self.hardware = Hardware(debug)
+        
         self.debug = debug
-        self.register = {i: 0 for i in range(0x10, 0x3C)}
+        self.register = [None for i in range(0x0, 0x3c)]
         self.memory = mem_sector
         self.memory.update(data_sector)
-
-
+        self.hardware = Hardware(self.memory, debug)
         sorted_items = sorted(code_sector.items())
         self.code_memory = tuple(value for key, value in sorted_items)
         self.code_addresses = {key: index for index, (key, value) in enumerate(sorted_items)}
         self.code_len = len(sorted_items)
-
+        
 
         self.flags = {'ZF': 0, 'CF': 0, 'SF': 0, 'OF': 0}
         self.pc = 0
@@ -72,13 +74,13 @@ cdef class Engine:
         cdef tuple op
         try:
             if self.debug:
-                while self.pc < self.code_len and not self.stop_event.is_set():
+                while self.running_conditions():
                     op = self.code_memory[self.pc]
-                    printf("[Execute] [Address:"+hex(self.pc)+"] "+op)
+                    print("[Execute] [Address:"+hex(self.pc)+"] "+op)
                     self.execute(op)
                     self.pc += 1
             else:
-                while self.pc < self.code_len and not self.stop_event.is_set():
+                while self.running_conditions():
                     op = self.code_memory[self.pc]
                     self.execute(op)
                     self.pc += 1
@@ -91,85 +93,85 @@ cdef class Engine:
             return (self.zip)
         return (self.zip)
 
-
+    cdef bint running_conditions(self):
+        return (self.pc < self.code_len and not self.stop_event.is_set())
     cdef void execute(self, tuple op):
-        cdef str opcode = op[0]
-        cdef bytes opcode_bytes = opcode.encode('utf-8')
-        cdef char* opcode_cstr = <char*>opcode_bytes
-        if strcmp(opcode_cstr, b'1b') == 0:
+        cdef unsigned char opcode = int(op[0], 16)
+
+        if opcode == 0x1b:
             self.add(op)
-        elif strcmp(opcode_cstr, b'1c') == 0:
+        elif opcode == 0x1c:
             self.sub(op)
-        elif strcmp(opcode_cstr, b'1d') == 0:
+        elif opcode == 0x1d:
             self.mul(op)
-        elif strcmp(opcode_cstr, b'1e') == 0:
+        elif opcode == 0x1e:
             self.div(op)
-        elif strcmp(opcode_cstr, b'1f') == 0:
+        elif opcode == 0x1f:
             self.mod(op)
-        elif strcmp(opcode_cstr, b'20') == 0:
+        elif opcode == 0x20:
             self.pow(op)
-        elif strcmp(opcode_cstr, b'2a') == 0:
+        elif opcode == 0x2a:
             self.and_op(op)
-        elif strcmp(opcode_cstr, b'2b') == 0:
+        elif opcode == 0x2b:
             self.or_op(op)
-        elif strcmp(opcode_cstr, b'2c') == 0:
+        elif opcode == 0x2c:
             self.xor(op)
-        elif strcmp(opcode_cstr, b'2d') == 0:
+        elif opcode == 0x2d:
             self.not_op(op)
-        elif strcmp(opcode_cstr, b'2e') == 0:
+        elif opcode == 0x2e:
             self.shl(op)
-        elif strcmp(opcode_cstr, b'2f') == 0:
+        elif opcode == 0x2f:
             self.shr(op)
-        elif strcmp(opcode_cstr, b'40') == 0:
+        elif opcode == 0x40:
             self.load(op)
-        elif strcmp(opcode_cstr, b'41') == 0:
+        elif opcode == 0x41:
             self.store(op)
-        elif strcmp(opcode_cstr, b'42') == 0:
+        elif opcode == 0x42:
             self.cmp(op)
-        elif strcmp(opcode_cstr, b'43') == 0:
+        elif opcode == 0x43:
             self.jmp(op)
-        elif strcmp(opcode_cstr, b'44') == 0:
+        elif opcode == 0x44:
             self.je(op)
-        elif strcmp(opcode_cstr, b'45') == 0:
+        elif opcode == 0x45:
             self.jne(op)
-        elif strcmp(opcode_cstr, b'46') == 0:
+        elif opcode == 0x46:
             self.jg(op)
-        elif strcmp(opcode_cstr, b'47') == 0:
+        elif opcode == 0x47:
             self.jl(op)
-        elif strcmp(opcode_cstr, b'48') == 0:
+        elif opcode == 0x48:
             self.mov(op)
-        elif strcmp(opcode_cstr, b'49') == 0:
+        elif opcode == 0x49:
             self.interrupt(op)
-        elif strcmp(opcode_cstr, b'4a') == 0:
+        elif opcode == 0x4a:
             self.change_mode(op)
-        elif strcmp(opcode_cstr, b'4b') == 0:
+        elif opcode == 0x4b:
             self.call(op)
-        elif strcmp(opcode_cstr, b'4c') == 0:
+        elif opcode == 0x4c:
             self.ret(op)
-        elif strcmp(opcode_cstr, b'4d') == 0:
+        elif opcode == 0x4d:
             self.push(op)
-        elif strcmp(opcode_cstr, b'4e') == 0:
+        elif opcode == 0x4e:
             self.pop(op)
-        elif strcmp(opcode_cstr, b'4f') == 0:
+        elif opcode == 0x4f:
             self.swap(op)
-        elif strcmp(opcode_cstr, b'50') == 0:
+        elif opcode == 0x50:
             self.dup(op)
-        elif strcmp(opcode_cstr, b'51') == 0:
+        elif opcode == 0x51:
             self.drop(op)
-        elif strcmp(opcode_cstr, b'52') == 0:
+        elif opcode == 0x52:
             self.halt(op)
-        elif strcmp(opcode_cstr, b'53') == 0:
+        elif opcode == 0x53:
             self.rand(op)
-        elif strcmp(opcode_cstr, b'54') == 0:
+        elif opcode == 0x54:
             self.inc(op)
-        elif strcmp(opcode_cstr, b'55') == 0:
+        elif opcode == 0x55:
             self.dec(op)
-        elif strcmp(opcode_cstr, b'56') == 0:
+        elif opcode == 0x56:
             self.abs(op)
-        elif strcmp(opcode_cstr, b'57') == 0:
+        elif opcode == 0x57:
             self.neg(op)
         else:
-            printf("Unknown opcode: %s\n", opcode_cstr)
+            printf("Unknown opcode: 0x%02x\n", opcode)
 
 
     cdef void add(self, tuple op): self.register[op[1]] += self.register[op[2]]
@@ -246,25 +248,12 @@ cdef class Engine:
 
 
     cdef void biscuit_call(self):
-        cdef bytes _debug_memory
-        cdef bytes _debug_stack
-        cdef bytes _debug_flags
-        cdef bytes _debug_pc
-        cdef bytes _debug_hardware_memory_address
         cdef int call = self.register[0x2f]
         if call == 0x00:
             arg1 = self.register[0x30]
             self.exit()
         elif call == 0x01:
-            hardware_memory = {}
-            for i in range(hardware_memory_addresses_len):
-                h_address = hardware_memory_addresses[h_address]
-                if self.debug:
-                    _debug_hardware_memory_address = bytes(str(h_address))
-                    printf("[UPDATE] Updating Hardware address: %s\n", _debug_hardware_memory_address)
-                hardware_memory[h_address] = self.memory[h_address]
-            result = self.hardware.update(hardware_memory)
-            self.memory.update(result)
+            result = self.hardware.update()
         elif call == 0x02:
             arg1 = self.register[0x30]/1000
             time.sleep(arg1)
@@ -279,17 +268,13 @@ cdef class Engine:
             arg2 = self.register[0x31]
     
             if arg1 == 0x01:
-                printf(arg2)
+                print(arg2)
         elif call == 0x05:
-            _debug_memory = bytes(str(self.memory))
-            _debug_stack = bytes(str(self.stack))
-            _debug_flags = bytes(str(self.flags))
-            _debug_pc = bytes(str(self.pc))
-            printf("Memory: %s\n", _debug_memory)
-            printf("Stack: %s\n", _debug_stack)
-            printf("Flags: %s\n", _debug_flags)
-            printf("Program Counter: %s\n", _debug_pc)
-            printf("Mode: %s\n", self.mode)
+            print(f"Memory: {self.memory}")
+            print(f"Stack: {self.stack}")
+            print(f"Flags: {self.flags}")
+            print(f"Program Counter: {self.pc}")
+            print(f"Mode: {self.mode}", )
         elif call == 0x06:
             arg1 = self.register[0x30]
             arg2 = self.register[0x31]
@@ -327,22 +312,37 @@ cdef class Engine:
 
 
     cdef void fs_read_file(self, str file):
-        with engine_lock_fs:
-            with zipfile.ZipFile(self.zip, "r", compression=zipfile.ZIP_DEFLATED) as zip:
-                self.register[0x2f] = zip.read(file)
+        engine_lock_fs.acquire()
+        
+        try:
+            z = zipfile.ZipFile(self.zip, "r", compression=zipfile.ZIP_DEFLATED)
+            self.register[0x2f] = z.read(file)  
+        finally:
+            engine_lock_fs.release()
+            z.close()
+
     cdef void fs_write_file(self, str file, text):
-        with engine_lock_fs:
-            with zipfile.ZipFile(self.zip, "a", compression=zipfile.ZIP_DEFLATED) as zip:
-                zip.writestr(file, text)
+        engine_lock_fs.acquire()
+        
+        try:
+            z = zipfile.ZipFile(self.zip, "a", compression=zipfile.ZIP_DEFLATED)
+            z.writestr(file, text) 
+        finally:
+            engine_lock_fs.release()
+            z.close()
             
     cdef void fs_exists_file(self, str file):
-        with engine_lock_fs:
-            with zipfile.ZipFile(self.zip, "r", compression=zipfile.ZIP_DEFLATED) as zip:
-                if file in zip.namelist():
-                    self.register[0x2f] = 1
-                else:
-                    self.register[0x2f] = 0
-
+        engine_lock_fs.acquire()
+        
+        try:
+            z = zipfile.ZipFile(self.zip, "r", compression=zipfile.ZIP_DEFLATED)
+            if file in z.namelist():
+                self.register[0x2f] = 1
+            else:
+                self.register[0x2f] = 0 
+        finally:
+            engine_lock_fs.release()
+            z.close()
 
 
 
@@ -507,12 +507,12 @@ cdef class Hardware:
     cdef dict hardware_memory
     cdef dict inet_connection
     cdef object debug
-    def __init__(self, debug):
-        self.hardware_memory = {}
+    def __init__(self, dict memory, debug):
+        self.hardware_memory = memory
+        self.hardware_memory[0xFFFF_0100] == 99
         self.debug = debug
         self.inet_connection = dict[int, socket.socket]()
-    cdef dict update(self, hardware_memory): #Hardware memory is the memory of the engine with 0xFFFF####
-        self.hardware_memory = hardware_memory
+    cdef dict update(self): #Hardware memory is the memory of the engine with 0xFFFF####
         self.update_color()
         self.internet()
 
@@ -553,7 +553,7 @@ cdef class Hardware:
         cdef int port
         cdef int kind
         cdef int fam
-        if action == None:
+        if action == 99:
             return
         else:
             port = int(self.hardware_memory[0xFFFF_0104])
@@ -566,7 +566,7 @@ cdef class Hardware:
                     fam = socket.AF_INET
                 else:
                     if self.debug:
-                        printf("Invalid family of socket: %s\n", fam)
+                        print(f"Invalid family of socket: {fam}")
                     return
                 if kind == 0x00: # UPD
                     kind = socket.SOCK_DGRAM
@@ -574,7 +574,7 @@ cdef class Hardware:
                     kind = socket.SOCK_STREAM
                 else:
                     if self.debug:
-                        printf("Invalid kind of socket: %s\n", kind)
+                        print(f"Invalid kind of socket: {kind}")
                     return
                 sock = socket.socket(fam, kind)
                 sock.bind((socket.gethostbyname(host), port))
@@ -588,7 +588,7 @@ cdef class Hardware:
                     fam = socket.AF_INET
                 else:
                     if self.debug:
-                        printf("Invalid family of socket: %s\n", fam)
+                        print(f"Invalid family of socket: {fam}")
                     return
                 if kind == 0x00: # UPD
                     kind = socket.SOCK_DGRAM
@@ -596,7 +596,7 @@ cdef class Hardware:
                     kind = socket.SOCK_STREAM
                 else:
                     if self.debug:
-                        printf("Invalid kind of socket: %s\n", kind)
+                        print(f"Invalid kind of socket: {kind}", kind)
                     return
                 sock = socket.socket(fam, kind)
                 sock.connect((socket.gethostbyname(host), port))
@@ -618,9 +618,8 @@ cdef class Hardware:
             elif action == 0x04: # exit
                 
                 self.inet_connection[port].close()
-        self.hardware_memory[0xFFFF_0100] = None
-                
-        
+        self.hardware_memory[0xFFFF_0100] = 99
+                   
         
 cdef class StopEngineInterrupt(Exception):
     pass
